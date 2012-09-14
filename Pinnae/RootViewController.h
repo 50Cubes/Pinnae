d9bb1342076b275ef2137ca3fd4c6d165dbebda8
Astrism <ghelton@gmail.com>
2012-09-13 19:17:58 -0700

    first!

---
+++ Pinnae/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Pinnae
+//
+//  Created by ampersand on 9/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
