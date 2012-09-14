f12eb3e936fa1465ff059ccfc7735015eafc984c
ronaldlee <ronaldylee@gmail.com>
2012-09-14 16:59:24 -0700

    hide image when enter next room, sound fix.

--- www/ResultConfig.js
+++ www/ResultConfig.js
@@ -2,7 +2,7 @@ var RESULTS_CONFIG = [
 	{
 		preSound: ['sound/creeek.mp3', 'sound/creeek.mp3', 'sound/creeek.mp3'],
 		soundDelay: 3000,
-		postSound: ['sound/scream.mp3', 'sound/scream.mp3', 'sound/scream.mp3'],
+		postSound: ['sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3'],
 		sprite: ['img/Monster_hamster1.jpg','img/Monster_hamster2.jpg'],
 		anxietyChange: 10,
 		inventoryItem: false
@@ -10,7 +10,7 @@ var RESULTS_CONFIG = [
 	{
 		preSound: ['sound/keys.mp3', 'sound/keys.mp3', 'sound/keys.mp3'],
 		soundDelay: 1000,
-		postSound: ['sound/snarl.mp3', 'sound/snarl.mp3', 'sound/snarl.mp3'],
+		postSound: ['sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3'],
 		sprite: ['img/Monster_tiger1.jpg','img/Monster_tiger2.jpg'],
 		anxietyChange: 20,
 		inventoryItem: false
@@ -18,7 +18,7 @@ var RESULTS_CONFIG = [
 	{
 		preSound: ['sound/snarl.mp3', 'sound/snarl.mp3', 'sound/snarl.mp3'],
 		soundDelay: 4000,
-		postSound: ['sound/keys.mp3', 'sound/keys.mp3', 'sound/keys.mp3'],
+		postSound: ['sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3'],
 		sprite: ['img/Monster_tiger1.jpg','img/Monster_tiger2.jpg'],
 		anxietyChange: 40,
 		inventoryItem: false
@@ -26,7 +26,7 @@ var RESULTS_CONFIG = [
 	{
 		preSound: ['sound/snarl.mp3', 'sound/snarl.mp3', 'sound/snarl.mp3'],
 		soundDelay: 4000,
-		postSound: ['sound/keys.mp3', 'sound/keys.mp3', 'sound/keys.mp3'],
+		postSound: ['sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3', 'sound/pre_flap_F.mp3'],
 		sprite: ['img/Monster_tiger1.jpg','img/Monster_tiger2.jpg'],
 		anxietyChange: -10,
 		inventoryItem: true	
