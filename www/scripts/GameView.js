704a0e3200d60a9f73a41d289d12b0a43a9e60d9
ronaldlee <ronaldylee@gmail.com>
2012-09-14 11:19:25 -0700

    fix speed variation and add random results

--- www/scripts/GameView.js
+++ www/scripts/GameView.js
@@ -24,7 +24,7 @@ var GameView = new Class(
 			html: 'Game View'
 		});
 		rep.adopt(title);
-
+		
 		//TODO: Anxiety Meter
 		//TODO: Inventory UI
 		//TODO: Result Selection UI (hide by default with class 'hide', revealed in enterRoom)
@@ -34,17 +34,36 @@ var GameView = new Class(
 	},
 	enterRoom: function()
 	{
+		console.log("enter room");
+		
 		// pick 3 random Results
 		//TODO: get random results (var rand = Number.random(minNum, maxNum);)
 		//TODO: set inventoryItem to true for 1 item (should only happen every 3 rooms...or reduce the chance so its around every 3 rooms) 
 		//		We will tweak this later to change the length of the game if its too long/short
 
+		var cloned_results = Array.clone(this.options.results);
+		
+		for (var i=0; i < 3; i++) {
+			var rand_result_index = Math.floor(Math.random()*1000) % cloned_results.length;
+			var rand_result = this.options.results[rand_result_index];
+			
+			cloned_results.splice(rand_result_index,1);
+			console.log("clone result length:" + cloned_results.length);
+			
+			var pre_sound = rand_result.options.preSound;
+			var sound_delay = rand_result.options.soundDelay;
+			
+			console.log("rand result index: " + rand_result_index + "; presound: " + pre_sound + "; sound_delay: "+sound_delay);
+			this.playSound(pre_sound, sound_delay);
+		}
 
 		//play sounds for this room
 		//TODO: use random results instead of hardcoded ones (result.preSound, result.soundDelay)
+		/*
 		this.playSound('sound/creeek.mp3', 2000);
 		this.playSound('sound/keys.mp3', 2500);
 		this.playSound('sound/snarl.mp3', 3000);
+		*/
 
 		//TODO: Unhide selection UI
 
@@ -56,9 +75,8 @@ var GameView = new Class(
 
 		//
 	},
-	playSound: function(soundFilePath, speed, variation)
+	playSound: function(soundFilePath, speed)
 	{
-		console.log("variation: " + variation);
 		//TODO: Separate starting the loop from playing the sound so that all three sounds dont play at once the first time
 	
 		if($(document.body).hasClass('device'))
@@ -74,9 +92,11 @@ var GameView = new Class(
 		//TODO: add a random variation(+/- 500ms) to the speed so that its not always the same repeat pattern
 		variation = Math.floor((Math.random()*1000)+1) - 500; //random between -500 to +500 msec
 		
+		speed += variation;
+		
 		setTimeout(function()
 		{
-			this.playSound(soundFilePath, speed, variation);
+			this.playSound(soundFilePath, speed);
 		}.bind(this), speed);
 
 		function mediaSuccess()
