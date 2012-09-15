2ce2a0132e0d242fcb9cda30f490b5d4aab06b37
Alec Ditonto <hiptonto@50cubes.com>
2012-09-14 23:24:05 -0700

    updated sounds to be distinct

--- www/ResultConfig.js
+++ www/ResultConfig.js
@@ -21,46 +21,26 @@ var RESULTS_CONFIG = {
 			inventoryItem: true
 		},
 		{
-			name: 'nibbler',
-			preSound: ['sound/nibbler_pre_L.mp3', 'sound/nibbler_pre_F.mp3', 'sound/nibbler_pre_R.mp3'],
-			soundDelay: 8000,
-			postSound: 'sound/nibbler_post_good.mp3',
+			name: 'drop',
+			preSound: ['sound/drop_pre_L.mp3', 'sound/drop_pre_F.mp3', 'sound/drop_pre_R.mp3'],
+			soundDelay: 6000,
+			postSound: 'sound/drop_post_good.mp3',
 			postDelay: 5000,
-			sprite: ['img/Grandpa_nibbler.jpg'],
+			sprite: ['img/Grandpa_toilet.jpg'],
 			anxietyChange: -10,
-			inventoryItem: false	
-		},
-		{
-			name: 'snarl',
-			preSound: ['sound/snarl_pre_L.mp3', 'sound/snarl_pre_F.mp3', 'sound/snarl_pre_R.mp3'],
-			soundDelay: 8000,
-			postSound: 'sound/snarl_post_good.mp3',
-			postDelay: 5000,
-			sprite: ['img/Grandpa_snarl.jpg'],
-			anxietyChange: -10,
-			inventoryItem: true	
+			inventoryItem: true
 		},
 
 		//Nuetral
 		{
-			name: 'bubbler',
-			preSound: ['sound/bubbler_pre_L.mp3', 'sound/bubbler_pre_F.mp3', 'sound/bubbler_pre_R.mp3'],
-			soundDelay: 6000,
-			postSound: 'sound/neutral.mp3',
+			name: 'flap',
+			preSound: ['sound/flap_pre_L.mp3', 'sound/flap_pre_F.mp3', 'sound/flap_pre_R.mp3'],
+			soundDelay: 8000,
+			postSound: 'sound/flap_post_good.mp3',
 			postDelay: 5000,
 			sprite: ['img/Neutral_1.jpg'],
 			anxietyChange: 0,
 			inventoryItem: false
-		},
-		{
-			name: 'drop',
-			preSound: ['sound/drop_pre_L.mp3', 'sound/drop_pre_F.mp3', 'sound/drop_pre_R.mp3'],
-			soundDelay: 6000,
-			postSound: 'sound/neutral.mp3',
-			postDelay: 5000,
-			sprite: ['img/Neutral_1.jpg'],
-			anxietyChange: 0,
-			inventoryItem: true
 		},
 		{
 			name: 'flap',
@@ -73,58 +53,19 @@ var RESULTS_CONFIG = {
 			inventoryItem: false
 		},
 		{
-			name: 'nibbler',
-			preSound: ['sound/nibbler_pre_L.mp3', 'sound/nibbler_pre_F.mp3', 'sound/nibbler_pre_R.mp3'],
+			name: 'flap',
+			preSound: ['sound/flap_pre_L.mp3', 'sound/flap_pre_F.mp3', 'sound/flap_pre_R.mp3'],
 			soundDelay: 8000,
-			postSound: 'sound/neutral.mp3',
-			postDelay: 5000,
-			sprite: ['img/Neutral_1.jpg'],
-			anxietyChange: -10,
-			inventoryItem: false	
-		},
-		{
-			name: 'snarl',
-			preSound: ['sound/snarl_pre_L.mp3', 'sound/snarl_pre_F.mp3', 'sound/snarl_pre_R.mp3'],
-			soundDelay: 8000,
-			postSound: 'sound/neutral.mp3',
+			postSound: 'sound/flap_post_good.mp3',
 			postDelay: 5000,
 			sprite: ['img/Neutral_1.jpg'],
 			anxietyChange: 0,
-			inventoryItem: true	
+			inventoryItem: false
 		}
 	],
 
 	bad: [
-		{
-			name: 'bubbler',
-			preSound: ['sound/bubbler_pre_L.mp3', 'sound/bubbler_pre_F.mp3', 'sound/bubbler_pre_R.mp3'],
-			soundDelay: 6000,
-			postSound: 'sound/bubbler_post_bad.mp3',
-			postDelay: 5000,
-			sprite: ['img/Monster_Witch1.jpg','img/Monster_Witch2.jpg'],
-			anxietyChange: 10,
-			inventoryItem: false
-		},
-		{
-			name: 'drop',
-			preSound: ['sound/drop_pre_L.mp3', 'sound/drop_pre_F.mp3', 'sound/drop_pre_R.mp3'],
-			soundDelay: 6000,
-			postSound: 'sound/drop_post_bad.mp3',
-			postDelay: 5000,
-			sprite: ['img/Monster_Toilet1.jpg','img/Monster_Toilet2.jpg'],
-			anxietyChange: 20,
-			inventoryItem: false
-		},
-		{
-			name: 'flap',
-			preSound: ['sound/flap_pre_L.mp3', 'sound/flap_pre_F.mp3', 'sound/flap_pre_R.mp3'],
-			soundDelay: 8000,
-			postSound: 'sound/flap_post_bad.mp3',
-			postDelay: 5000,
-			sprite: ['img/Monster_Bat1.jpg','img/Monster_Bat2.jpg'],
-			anxietyChange: 40,
-			inventoryItem: false
-		},
+
 		{
 			name: 'nibbler',
 			preSound: ['sound/nibbler_pre_L.mp3', 'sound/nibbler_pre_F.mp3', 'sound/nibbler_pre_R.mp3'],
@@ -132,6 +73,16 @@ var RESULTS_CONFIG = {
 			postSound: 'sound/nibbler_post_bad.mp3',
 			postDelay: 5000,
 			sprite: ['img/Monster_hamster1.jpg','img/Monster_hamster2.jpg '],
+			anxietyChange: 10,
+			inventoryItem: false	
+		},
+		{
+			name: 'snarl',
+			preSound: ['sound/snarl_pre_L.mp3', 'sound/snarl_pre_F.mp3', 'sound/snarl_pre_R.mp3'],
+			soundDelay: 8000,
+			postSound: 'sound/snarl_post_bad.mp3',
+			postDelay: 5000,
+			sprite: ['img/Monster_tiger1.jpg','img/Monster_tiger2.jpg'],
 			anxietyChange: 10,
 			inventoryItem: false	
 		},
