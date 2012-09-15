befcb969a4e10ab2d4acd0b5b3c60dbd8801baa7
Ethan Kan <ethankan@50cubes.com>
2012-09-14 22:03:29 -0700

    Fixed game over screen.

--- www/scripts/GameView.js
+++ www/scripts/GameView.js
@@ -115,6 +115,12 @@ var GameView = new Class(
 		else
 		{
 			console.log("========== HIT BY Monster replay same room!!!! ========");
+			if (this.options.player.options.anxiety >= 100)
+			{
+				this.onGameOver();
+				return;
+			}
+
 			for(var i = 0; i < this.options.roomResults.length; i++)
 			{
 			  this.playSound(this.options.roomResults[i].options.preSound[i], this.options.roomResults[i].options.soundDelay, true);	
@@ -210,7 +216,7 @@ var GameView = new Class(
 			}.bind(this), 2000);
 		}
 
-	  	this.playSound(result.options.postSound, 0, false);
+	  this.playSound(result.options.postSound, 0, false);
 		
 		var player = this.options.player;	
 		if(result.options.inventoryItem)
@@ -234,13 +240,7 @@ var GameView = new Class(
 		//update meter
 		$('meter').setStyle('height', player.options.anxiety + '%');
 
-		if (player.options.anxiety >= 100)
-		{
-			this.onGameOver();
-			return;
-		}
 		setTimeout(this.enterRoom.bind(this), result.options.postDelay);
-
 	},
 	playRevealImage: function(image_url)
 	{
